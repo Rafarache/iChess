9c43cc7e19097969f872a9fa20d12c7f868ee653
rrache <rR@che#2k20>
2021-03-06 21:21:42 -0300

    Create board

--- Chess/ContentView.swift
+++ Chess/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    var colorOne: Color = .black
+    var colorTwo: Color = .white
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 0) {
+            ForEach(0..<8) { row in
+                HStack(spacing: 0) {
+                    ForEach(0..<8) { collum in
+                        VStack(spacing: 0) {
+                            if isOddNumber(number: row) {
+                                Rectangle()
+                                    .foregroundColor(isOddNumber(number: collum) ? colorOne : colorTwo)
+                                    .frame(width: 100, height: 100)
+                            }
+                            else {
+                                Rectangle()
+                                    .foregroundColor(isOddNumber(number: collum) ? colorTwo : colorOne)
+                                    .frame(width: 100, height: 100)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func isOddNumber(number: Int) -> Bool {
+        return number % 2 == 1
     }
 }
 
