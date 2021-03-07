bf0d53b8fc0a3a5cebcd901d0ee3f0c22e398055
rrache <rR@che#2k20>
2021-03-06 21:00:00 -0300

    Initial Commit

---
+++ Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Nelogica on 06/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
